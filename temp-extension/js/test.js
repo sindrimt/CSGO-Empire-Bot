0813552c11494d929a3410021b909b4bfd0052ab
sindrimt <sindrimt@gmail.com>
2021-08-14 22:25:39 +0200

    Yup

--- temp-extension/js/test.js
+++ temp-extension/js/test.js
@@ -1,17 +1,23 @@
-/*bodyEL = document.querySelector("body");
-var counter = 0;
-btn = document.querySelector("button");
-btn.addEventListener(
-  "click",
-  function () {
-    console.log(counter);
-    console.log("safsd");
-    counter++;
-    divv = document.createElement("div");
-    divv.style.backgroundColor = "red";
-    divv.style.width = 200 + "px";
-    divv.style.height = 200 + "px";
-    bodyEL.appendChild(divv);
-  },
-  false
-);*/
+// ctrl + option + n for å kjøre
+// Dette er et shit forsøk på en algo som deler CurrentBet inn i tideler av currentBet,
+// Slik at det blir færre knapper å trykke på :P
+
+var int = 0.66;
+i = 1000000;
+while (i != 0) {
+  if (~~(int / 0.1) >= 1) {
+    result = ~~(int / 0.1);
+    console.log("Times 0.1: " + result);
+    rest = Math.ceil((int - result * 0.1) * 10000) / 10000;
+    console.log("The rest: " + rest);
+    i = rest;
+  }
+
+  if (~~(rest / 0.01) >= 1) {
+    result2 = ~~(rest / 0.01);
+    console.log("Times the rest of 0.01: " + result2);
+    rest2 = Math.ceil((rest - result2 * 0.01) * 100000) / 100000;
+    //console.log("The rest: " + rest2);
+    i = rest2;
+  }
+}
